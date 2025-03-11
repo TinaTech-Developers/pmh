7dfcc73685ee89196a0e073ab68db5c049aa0a44
TinaTech-Developers <tinashephiri0@gmail.com>
2025-03-11 12:14:49 +0200

    heros

--- app/components/PageHero.tsx
+++ app/components/PageHero.tsx
@@ -6,15 +6,15 @@ interface PageHeroProps {
 }
 function PageHero({ title }: PageHeroProps) {
   return (
-    <div className="w-full h-[400px] md:h-[500px]">
+    <div className="w-full h-[500px] md:h-[500px]">
       <Image
         src={"/gazebo.jpg"}
         alt=""
         width={1024}
         height={800}
-        className="relative w-full  h-[400px] md:h-[500px] object-cover"
+        className="relative w-full  h-[500px] md:h-[500px] object-cover"
       />
-      <p className="absolute bottom-48 p-10 text-white font-semibold text-xl">
+      <p className="absolute bottom-64 p-10 text-white font-semibold text-xl">
         Home |{" "}
         <span className="font-normal text-gray-300 text-lg">{title}</span>
       </p>
--- app/contact-us/components/Hero.tsx
+++ app/contact-us/components/Hero.tsx
@@ -4,27 +4,28 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 function Hero() {
   return (
-    <div className="w-full h-[400px] md:h-[500px]">
+    <div className="w-full h-[500px] md:h-[500px]">
       <Image
         src={"/contact.png"}
         alt=""
         width={1024}
         height={800}
-        className="relative w-full h-[400px] md:h-[500px] object-cover"
+        className="relative w-full h-[500px] md:h-[500px] object-cover"
       />
-      <div className="absolute text-white bottom-48 p-4 md:px-10">
+      <div className="absolute text-white bottom-64 p-4 md:px-10">
         <h1 className="text-2xl font-bold md:text-5xl">Get in Touch with Us</h1>
-        <p className="mt-1"></p>
-        <Typewriter
-          options={{
-            autoStart: true,
-            loop: true,
-            delay: 50,
-            strings: [
-              "We're here to help. Whether you have a question, feedback, or need assistance, feel free to contact",
-            ],
-          }}
-        />
+        <p className="mt-1 text-sm">
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+              delay: 50,
+              strings: [
+                "We're here to help. Whether you have a question, feedback, or need assistance, feel free to contact",
+              ],
+            }}
+          />
+        </p>
       </div>
     </div>
   );
